8d273961c3478662befa9e25d40c1b5fe4d5ad59
naot97 <viettoan.bk1997@gmail.com>
2019-05-03 09:39:35 +0700

    add lab12

--- Lab11/info.php
+++ Lab11/info.php
@@ -1,24 +1,32 @@
-<?php
-session_start();
-if(isset($_SESSION["username"])){
-	echo "Username : ".$_SESSION["username"]."<br>";
-	echo "Password : ".$_SESSION["password"]."<br>";
-}
-else{
-	header('Location: login.php');
-}
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+<title>Info</title>
+</head>
+<body>
 <script >
- $('.button').click(function() {
-
- $.ajax({
-  type: "POST",
-  url: "some.php",
-  data: { name: "John" }
-}).done(function( msg ) {
-  alert( "Data Saved: " + msg );
-});    
-
-    });
+ function logout() {
+ 	sessionStorage.removeItem("username");
+ 	window.location = "index.php";
+ }
 </script>
+<?php
+	session_start();
+	if(isset($_SESSION["username"])){
+		echo "Username : ".$_SESSION["username"]."<br>";
+		echo "Password : ".$_SESSION["password"]."<br>";
+	}
+	else{
+		header('Location: login.php');
+	}
+?>
+<button id='logout' onclick="logout()">Log out</button>
+</body>
+</html>
+
+
+
+
+
 
